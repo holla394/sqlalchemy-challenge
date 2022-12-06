a4ff45c32ef77a2d5bbbb9bdd8e1b79c6c5407b3
holla394 <samuel.holland@emerson.com>
2022-12-06 13:07:14 -0600

    fixing graphs and adding to app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -28,7 +28,6 @@ Station = Base.classes.station
 #################################################
 app = Flask(__name__)
 
-
 #################################################
 # Flask Routes
 #################################################
@@ -39,9 +38,9 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/temperature<br/><br/>"
-        f"/api/v1.0/&lt;start_date&gt;<br/><br/>"
-        f"/api/v1.0/&lt;start_date&gt;/&lt;end_date&gt;<br/><br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start_date&gt;<br/>"
+        f"/api/v1.0/&lt;start_date&gt;/&lt;end_date&gt;<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -121,7 +120,7 @@ def tobs():
 # this is a decorator
 @app.route("/api/v1.0/<start_date>")
 def start_only(start_date):
-    end = request.args.get("end")
+    # end = request.args.get("end")
 
     with Session(engine) as session:
         # Design a query to find the most active stations (i.e. what stations have the most rows?)
@@ -131,27 +130,14 @@ def start_only(start_date):
         # List the stations and the counts in descending order.
         most_active = active_stations[0][0]
 
-        min = session.query(func.min(Measurement.tobs)).\
+        data = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
             where(Measurement.date >= start_date,\
-                Measurement.station == most_active,\
-                    Measurement.date <= end).\
-                        all()
+                Measurement.station == most_active).\
+                    all()
 
-        max = session.query(func.max(Measurement.tobs)).\
-            where(Measurement.date >= start_date,\
-                Measurement.station == most_active,\
-                    Measurement.date <= end).\
-                        all()
-
-        avg = session.query(func.avg(Measurement.tobs)).\
-            where(Measurement.date >= start_date,\
-                Measurement.station == most_active,\
-                    Measurement.date <= end).\
-                        all()
-
-    min = min[0][0]
-    max = max[0][0]
-    avg = avg[0][0]
+    min = data[0][0]
+    max = data[0][1]
+    avg = round(data[0][2], 2)
     # Convert list of tuples into normal list
     
     return jsonify({"Min temp":min,"Max temp":max,"Avg temp":avg})
@@ -159,7 +145,26 @@ def start_only(start_date):
 @app.route("/api/v1.0/<start_date>/<end_date>")
 def start_and_end(start_date, end_date):
 
-    return start_date + " " + end_date
+    with Session(engine) as session:
+        # Design a query to find the most active stations (i.e. what stations have the most rows?)
+        active_stations = session.query(Station.station, func.count(Station.station))\
+            .group_by(Station.station).order_by(func.count(Station.station).desc()).all()
+
+        # List the stations and the counts in descending order.
+        most_active = active_stations[0][0]
+
+        data = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
+            where(Measurement.date >= start_date, Measurement.date <= end_date,\
+                Measurement.station == most_active).\
+                    all()
+
+    min = data[0][0]
+    max = data[0][1]
+    avg = round(data[0][2], 2)
+    # Convert list of tuples into normal list
+    
+    return jsonify({"Min temp":min,"Max temp":max,"Avg temp":avg})
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
